7bbc4ffcd26aa8c722090c6083e8074175e61d12
Sarah <sarah.c.abarbanel@gmail.com>
2026-01-27 14:38:02 -0500

    Starting work on script, getting headers and keys (github secrets)

---
+++ flashfood_notifier.py
@@ -0,0 +1,25 @@
+#polls flashfood and sends telegram bot notifications according to criteria.
+import os
+
+# Keeping these as secrets in case they're sensitive
+def get_flashfood_config() -> tuple[str, dict]:
+    
+    #Gets Flashfood API fields and headers from environment variables
+    api_url = os.environ.get("FLASHFOOD_API_URL")
+    api_key = os.environ.get("FLASHFOOD_API_KEY")
+    app_info = os.environ.get("FLASHFOOD_APP_INFO")
+
+    if not all([api_url, api_key, app_info]):
+        raise ValueError(
+            "Missing flashfood environment variables: "
+            "FLASHFOOD_API_URL, FLASHFOOD_API_KEY, FLASHFOOD_APP_INFO"
+        )
+
+    headers = {
+        "accept": "application/json",
+        "x-ff-api-key": api_key,
+        "flashfood-app-info": app_info,
+        "User-Agent": "okhttp/4.12.0"
+    }
+
+    return api_url, headers
